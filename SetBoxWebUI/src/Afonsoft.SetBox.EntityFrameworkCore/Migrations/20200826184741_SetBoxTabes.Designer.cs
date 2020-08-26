d53fcda483f40b23f5db1653242857de06b8a510
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2020-08-26 15:54:27 -0300

    Criação das tabelas do SetBox

--- SetBoxWebUI/src/Afonsoft.SetBox.EntityFrameworkCore/Migrations/20200826184741_SetBoxTabes.Designer.cs
+++ SetBoxWebUI/src/Afonsoft.SetBox.EntityFrameworkCore/Migrations/20200826184741_SetBoxTabes.Designer.cs
@@ -4,14 +4,16 @@ using Afonsoft.SetBox.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Afonsoft.SetBox.Migrations
 {
     [DbContext(typeof(SetBoxDbContext))]
-    partial class SetBoxDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200826184741_SetBoxTabes")]
+    partial class SetBoxTabes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
